8e180ae9e86f1ae9a45c8c870c3fcd83e1fcc988
Reiga22 <riantoriki685@gmail.com>
2022-07-19 16:39:06 +0700

    web-react-saya

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: '' };
-  }
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value});
-  }
-  render() {
-    return (
-      <form>
-      <h3> {this.state.username}</h3>
-      <p>Masakuan Kata Kunci:</p>
-      <input
-        type='text'
-        onChange={this.myChangeHandler}
-      />
-      </form>
-    );
-  }
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<MyForm />, document.getElementById('root'));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
